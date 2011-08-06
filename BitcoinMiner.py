78d21377e6a2daec486e635bdf724e58641152c7
Jason Lynch <jason@calindora.com>
2011-08-05 21:30:42 -0500

    Remove MtRed fee, as it doesn't seem to be implemented (yet).
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- BitcoinMiner.py
+++ BitcoinMiner.py
@@ -81,6 +81,7 @@ class BitcoinMiner():
 			if self.stale:
 				self.stale = False
 				work = None
+				print('Dropping stale shares early')
 
 			if (not work) or (not self.work_queue.empty()):
 				try:
@@ -104,6 +105,7 @@ class BitcoinMiner():
 			if self.stale:
 				self.stale = False
 				work = None
+				print('Dropping stale shares late')
 				continue
 
 			nonces_left -= global_threads
